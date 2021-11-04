074736b09c636203177b543fff8162ddbb4fb4e1
Vladimir Sterlin <vsterlin21@gmail.com>
2021-11-03 23:46:27 -0700

    comment for future

--- Casino.sol
+++ Casino.sol
@@ -4,10 +4,11 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Casino {
   address dealer;
-  address player;
 
   constructor(){
     dealer = msg.sender;
   }
 
+  // function play that will take in address of player
+  // determine winner and pay to dealer or winner
 }
